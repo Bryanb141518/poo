755b25a7896dfa6e7d5517871d0bf98f136d28d1
brayan benitez <bryan141518@gmail.com>
2025-02-26 11:14:15 -0500

    primer coambio poo

---
+++ poo.py
@@ -0,0 +1,24 @@
+class Estudiante:
+    def __init__(self,nombre,edad,grado):
+        self.nombre = nombre
+        self.edad = edad
+        self.grado = grado
+    def estudiar(self):
+        print("estudiando..")    
+        
+nombre = input("ingrese su nombre: ")
+edad = input("ingrese su edad: ")
+grado = input("ingrese su grado: ")
+sueldo = input("ingrese su sualedo")
+
+
+
+
+
+#herencia
+class Maestro (Estudiante):
+    def __init__(self, nombre, edad, grado,sueldo):
+        super().__init__(nombre, edad, grado)
+        self.sueldo = sueldo
+
+roberto = Maestro()
