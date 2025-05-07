228076d0406adaadad5a677d3ffad89dcdafea36
brayan benitez <bryan141518@gmail.com>
2025-05-07 13:06:52 -0500

    ejercicio con bucles

--- bucle.py
+++ bucle.py
@@ -1,22 +1,14 @@
-numero = int(input("Ingresa un numero: "))
-contador = 1
-while contador <= numero:
-    print(contador)
-    contador += 1
-
-num = int(input("Ingresa un numero: "))
-while num :
-    if num % 2 == 1:
-        print("El numero es impar")
+contador = 0
+contado =0
+while True:
+    numero = int(input("Ingresa un numero: "))
+    if numero == 0:
+        print("El numero es 0 el programa a terminado")
         break
-    else:
-        print("El numero es par")
-        break
+    if numero < 0:
+        contador += 1
+    elif numero > 0:
+        contado += 1
+print(f"el total de los numeros negativos ingresados fue de   {contador}")
+print (f"el total de numeros positivos  ingresados fue de  {contado}")
 
-num1 = int(input("adivina un numero entre 1 y 100: "))
-num2 = 32
-
-while num1 != num2:
-    print("El numero es incorrecto")
-    num1 = int(input("adivina un numero entre 1 y 100: "))
-print("El numero es correcto")
